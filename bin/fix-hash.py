64f877fd16b594ea9b3dbdf9d0479b1663aa50b2
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-04-30 13:15:42 +0800

    fix

--- bin/fix-hash.py
+++ bin/fix-hash.py
@@ -1,6 +1,16 @@
 import os,json
 from urllib.request import urlopen
 
+def fileReplace(file, old_str, new_str):
+    file_data = ""
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if old_str in line:
+                line = line.replace(old_str,new_str)
+            file_data += line
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(file_data)
+
 workdir = os.getcwd()
 cfwdir = workdir + os.sep + 'bucket' + os.sep + 'clash-for-windows.json'
 
@@ -12,10 +22,5 @@ with urlopen('https://github.com/Fndroid/clash_for_windows_pkg/releases/download
 	cfw_hash = sha256sum.split('7z: ')[1].split('\n')[0].lower()
 
 if cfw_hash != manifest['hash']:
-	with open(cfwdir, 'r+') as f:
-		d = f.read()
-		t = d.replace(manifest['hash'], cfw_hash)
-		f.seek(0, 0)
-		f.write(t)
-
+    fileReplace(cfwdir, manifest['hash'], cfw_hash)
 
