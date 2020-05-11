d2b05f00c157e7186b1574c4b8b0d690a31f3dfa
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-05-12 00:25:40 +0800

    Update

--- bin/fix-hash.py
+++ bin/fix-hash.py
@@ -1,26 +1,61 @@
-import os,json
-from urllib.request import urlopen
-
-def fileReplace(file, old_str, new_str):
-    file_data = ""
-    with open(file, "r", encoding="utf-8") as f:
-        for line in f:
-            if old_str in line:
-                line = line.replace(old_str,new_str)
-            file_data += line
-    with open(file, "w", encoding="utf-8") as f:
-        f.write(file_data)
+import os
+import json
+import requests
+import hashlib
 
 workdir = os.getcwd()
+
+def getHash(filename):
+   h = hashlib.sha256()
+
+   with open(filename,'rb') as file:
+       chunk = 0
+       while chunk != b'':
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   return h.hexdigest()
+
+# Clash
 cfwdir = workdir + os.sep + 'bucket' + os.sep + 'clash-for-windows.json'
 
 with open(cfwdir, 'r') as f:
-	manifest = json.load(f)
+    manifest = json.load(f)
 
-with urlopen('https://github.com/Fndroid/clash_for_windows_pkg/releases/download/' + manifest['version'] + '/sha256sum') as response:
-	sha256sum = response.read().decode('utf-8')
-	cfw_hash = sha256sum.split('7z: ')[1].split('\n')[0].lower()
+with requests.get('https://github.com/Fndroid/clash_for_windows_pkg/releases/download/' + manifest['version'] + '/sha256sum') as r:
+    sha256sum = r.text
+    cfw_hash = sha256sum.split('7z: ')[1].split('\n')[0].lower()
 
 if cfw_hash != manifest['hash']:
-    fileReplace(cfwdir, manifest['hash'], cfw_hash)
+    manifest['hash'] = cfw_hash
 
+with open(cfwdir, 'w') as f:
+    json.dump(manifest, f, indent=4, separators=(',', ': '))
+
+# Netch
+ndir = workdir + os.sep + 'bucket' + os.sep + 'netch.json'
+
+with open(ndir, 'r') as f:
+    manifest = json.load(f)
+
+with requests.get('https://api.github.com/repos/NetchX/Netch/releases/latest') as r:
+    n = r.json()
+    tag = n['tag_name']
+    url = n['assets'][0]['browser_download_url']
+
+if tag != manifest['version']:
+    manifest['version'] = tag
+    manifest['url'] = url
+    tmp = 'tmp'
+    with requests.get(url) as r:
+        with open(tmp, 'wb') as f:
+            f.write(r.content)
+    n_hash = getHash(tmp)
+    os.remove(tmp) 
+            
+    manifest['hash'] = n_hash
+
+print(manifest)
+
+with open(ndir, 'w') as f:
+    json.dump(manifest, f, indent=4, separators=(',', ': '))
