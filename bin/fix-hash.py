6b32b45fa14448b47e3f13316db7410c944333cd
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-10-30 14:09:43 +0800

    Update

--- bin/fix-hash.py
+++ bin/fix-hash.py
@@ -5,46 +5,55 @@ import hashlib
 
 workdir = os.getcwd()
 
+
 def getHash(file):
     h = hashlib.sha256()
     h.update(file)
 
     return h.hexdigest()
 
-# Clash
-cfwdir = workdir + os.sep + 'bucket' + os.sep + 'clash-for-windows.json'
 
-with open(cfwdir, 'r') as f:
+# Clash
+cfwdir = workdir + os.sep + "bucket" + os.sep + "clash-for-windows.json"
+
+with open(cfwdir, "r") as f:
     manifest = json.load(f)
 
-with requests.get('https://github.com/Fndroid/clash_for_windows_pkg/releases/download/' + manifest['version'] + '/Clash.for.Windows.Setup.' + manifest['version'] + '.exe') as r:
-    cfw_hash = getHash(r.content)
+with requests.get(manifest["architecture"]["64bit"]["url"].replace("#/dl.7z", "")) as r:
+    cfw64_hash = getHash(r.content)
 
-if cfw_hash != manifest['hash']:
-    manifest['hash'] = cfw_hash
+with requests.get(manifest["architecture"]["32bit"]["url"].replace("#/dl.7z", "")) as r:
+    cfw32_hash = getHash(r.content)
 
-    with open(cfwdir, 'w') as f:
-        json.dump(manifest, f, indent=4, separators=(',', ': '))
+if (
+    cfw64_hash != manifest["architecture"]["64bit"]["hash"]
+    or cfw32_hash != manifest["architecture"]["32bit"]["hash"]
+):
+    manifest["architecture"]["64bit"]["hash"] = cfw64_hash
+    manifest["architecture"]["32bit"]["hash"] = cfw32_hash
+
+    with open(cfwdir, "w") as f:
+        json.dump(manifest, f, indent=4, separators=(",", ": "))
 
 # Netch
-ndir = workdir + os.sep + 'bucket' + os.sep + 'netch.json'
+ndir = workdir + os.sep + "bucket" + os.sep + "netch.json"
 
-with open(ndir, 'r') as f:
+with open(ndir, "r") as f:
     manifest = json.load(f)
 
-with requests.get('https://api.github.com/repos/NetchX/Netch/releases/latest') as r:
+with requests.get("https://api.github.com/repos/NetchX/Netch/releases/latest") as r:
     n = r.json()
-    tag = n['tag_name']
-    url = n['assets'][0]['browser_download_url']
+    tag = n["tag_name"]
+    url = n["assets"][0]["browser_download_url"]
 
-if tag != manifest['version']:
-    manifest['version'] = tag
-    manifest['url'] = url
+if tag != manifest["version"]:
+    manifest["version"] = tag
+    manifest["url"] = url
 
     with requests.get(url) as r:
         n_hash = getHash(r.content)
-            
-    manifest['hash'] = n_hash
 
-    with open(ndir, 'w') as f:
-        json.dump(manifest, f, indent=4, separators=(',', ': '))
+    manifest["hash"] = n_hash
+
+    with open(ndir, "w") as f:
+        json.dump(manifest, f, indent=4, separators=(",", ": "))
